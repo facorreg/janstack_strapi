54024dab77f7c0ae4cb52a82cf1862089d2738a7
root <root@DESKTOP-51MGIR3.localdomain>
2020-12-16 10:57:27 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1c63e26c3823f576db3d25a6b992b814'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1442b5ca-ef0d-455d-91bd-a352366bc08d'
+};
